29655818feb627c7dd39a278b0f0241eef33d252
elgunakbarli <elgunekberli200@gmail.com>
2025-10-22 11:57:13 +0400

    client + server

---
+++ client/src/App.jsx
@@ -0,0 +1,140 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="">
+      <p className="text-red-600 font-bold">
+        aasfadsfsdafdsaf SDAFSADVSA VVSDVSD SDFVCDSAVDSVDSAVDSAV SDAV
+        SDAVS23423EDSFC əƏəəAssÜsssĞğĞ,ŞÇÇIııƏppPOl
+      </p>
+      <p>asadfasd</p>
+      <p className="text-red-600 font-bold">
+        aasfadsfsdafdsaf SDAFSADVSA VVSDVSD SDFVCDSAVDSVDSAVDSAV SDAV
+        SDAVS23423EDSFC əƏəəAssÜsssĞğĞ,ŞÇÇIııƏppPOl
+      </p>
+      <p>asadfasd</p>
+      <p className="text-red-600 font-bold">
+        aasfadsfsdafdsaf SDAFSADVSA VVSDVSD SDFVCDSAVDSVDSAVDSAV SDAV
+        SDAVS23423EDSFC əƏəəAssÜsssĞğĞ,ŞÇÇIııƏppPOl
+      </p>
+      <p>asadfasd</p>
+      <p className="text-red-600 font-bold">
+        aasfadsfsdafdsaf SDAFSADVSA VVSDVSD SDFVCDSAVDSVDSAVDSAV SDAV
+        SDAVS23423EDSFC əƏəəAssÜsssĞğĞ,ŞÇÇIııƏppPOl
+      </p>
+      <p>asadfasd</p>
+      <p className="text-red-600 font-bold">
+        aasfadsfsdafdsaf SDAFSADVSA VVSDVSD SDFVCDSAVDSVDSAVDSAV SDAV
+        SDAVS23423EDSFC əƏəəAssÜsssĞğĞ,ŞÇÇIııƏppPOl
+      </p>
+      <p>asadfasd</p>
+      <p className="text-red-600 font-bold">
+        aasfadsfsdafdsaf SDAFSADVSA VVSDVSD SDFVCDSAVDSVDSAVDSAV SDAV
+        SDAVS23423EDSFC əƏəəAssÜsssĞğĞ,ŞÇÇIııƏppPOl
+      </p>
+      <p>asadfasd</p>
+      <p className="text-red-600 font-bold">
+        aasfadsfsdafdsaf SDAFSADVSA VVSDVSD SDFVCDSAVDSVDSAVDSAV SDAV
+        SDAVS23423EDSFC əƏəəAssÜsssĞğĞ,ŞÇÇIııƏppPOl
+      </p>
+      <p>asadfasd</p>
+      <p className="text-red-600 font-bold">
+        aasfadsfsdafdsaf SDAFSADVSA VVSDVSD SDFVCDSAVDSVDSAVDSAV SDAV
+        SDAVS23423EDSFC əƏəəAssÜsssĞğĞ,ŞÇÇIııƏppPOl
+      </p>
+      <p>asadfasd</p>
+      <p className="text-red-600 font-bold">
+        aasfadsfsdafdsaf SDAFSADVSA VVSDVSD SDFVCDSAVDSVDSAVDSAV SDAV
+        SDAVS23423EDSFC əƏəəAssÜsssĞğĞ,ŞÇÇIııƏppPOl
+      </p>
+      <p>asadfasd</p>
+      <p className="text-red-600 font-bold">
+        aasfadsfsdafdsaf SDAFSADVSA VVSDVSD SDFVCDSAVDSVDSAVDSAV SDAV
+        SDAVS23423EDSFC əƏəəAssÜsssĞğĞ,ŞÇÇIııƏppPOl
+      </p>
+      <p>asadfasd</p>
+      <p className="text-red-600 font-bold">
+        aasfadsfsdafdsaf SDAFSADVSA VVSDVSD SDFVCDSAVDSVDSAVDSAV SDAV
+        SDAVS23423EDSFC əƏəəAssÜsssĞğĞ,ŞÇÇIııƏppPOl
+      </p>
+      <p>asadfasd</p>
+      <p className="text-red-600 font-bold">
+        aasfadsfsdafdsaf SDAFSADVSA VVSDVSD SDFVCDSAVDSVDSAVDSAV SDAV
+        SDAVS23423EDSFC əƏəəAssÜsssĞğĞ,ŞÇÇIııƏppPOl
+      </p>
+      <p>asadfasd</p>
+      <p className="text-red-600 font-bold">
+        aasfadsfsdafdsaf SDAFSADVSA VVSDVSD SDFVCDSAVDSVDSAVDSAV SDAV
+        SDAVS23423EDSFC əƏəəAssÜsssĞğĞ,ŞÇÇIııƏppPOl
+      </p>
+      <p>asadfasd</p>
+      <p className="text-red-600 font-bold">
+        aasfadsfsdafdsaf SDAFSADVSA VVSDVSD SDFVCDSAVDSVDSAVDSAV SDAV
+        SDAVS23423EDSFC əƏəəAssÜsssĞğĞ,ŞÇÇIııƏppPOl
+      </p>
+      <p>asadfasd</p>
+      <p className="text-red-600 font-bold">
+        aasfadsfsdafdsaf SDAFSADVSA VVSDVSD SDFVCDSAVDSVDSAVDSAV SDAV
+        SDAVS23423EDSFC əƏəəAssÜsssĞğĞ,ŞÇÇIııƏppPOl
+      </p>
+      <p>asadfasd</p>
+      <p className="text-red-600 font-bold">
+        aasfadsfsdafdsaf SDAFSADVSA VVSDVSD SDFVCDSAVDSVDSAVDSAV SDAV
+        SDAVS23423EDSFC əƏəəAssÜsssĞğĞ,ŞÇÇIııƏppPOl
+      </p>
+      <p>asadfasd</p>
+      <p className="text-red-600 font-bold">
+        aasfadsfsdafdsaf SDAFSADVSA VVSDVSD SDFVCDSAVDSVDSAVDSAV SDAV
+        SDAVS23423EDSFC əƏəəAssÜsssĞğĞ,ŞÇÇIııƏppPOl
+      </p>
+      <p>asadfasd</p>
+      <p className="text-red-600 font-bold">
+        aasfadsfsdafdsaf SDAFSADVSA VVSDVSD SDFVCDSAVDSVDSAVDSAV SDAV
+        SDAVS23423EDSFC əƏəəAssÜsssĞğĞ,ŞÇÇIııƏppPOl
+      </p>
+      <p>asadfasd</p>
+      <p className="text-red-600 font-bold">
+        aasfadsfsdafdsaf SDAFSADVSA VVSDVSD SDFVCDSAVDSVDSAVDSAV SDAV
+        SDAVS23423EDSFC əƏəəAssÜsssĞğĞ,ŞÇÇIııƏppPOl
+      </p>
+      <p>asadfasd</p>
+      <p className="text-red-600 font-bold">
+        aasfadsfsdafdsaf SDAFSADVSA VVSDVSD SDFVCDSAVDSVDSAVDSAV SDAV
+        SDAVS23423EDSFC əƏəəAssÜsssĞğĞ,ŞÇÇIııƏppPOl
+      </p>
+      <p>asadfasd</p>
+      <p className="text-red-600 font-bold">
+        aasfadsfsdafdsaf SDAFSADVSA VVSDVSD SDFVCDSAVDSVDSAVDSAV SDAV
+        SDAVS23423EDSFC əƏəəAssÜsssĞğĞ,ŞÇÇIııƏppPOl
+      </p>
+      <p>asadfasd</p>
+      <p className="text-red-600 font-bold">
+        aasfadsfsdafdsaf SDAFSADVSA VVSDVSD SDFVCDSAVDSVDSAVDSAV SDAV
+        SDAVS23423EDSFC əƏəəAssÜsssĞğĞ,ŞÇÇIııƏppPOl
+      </p>
+      <p>asadfasd</p>
+      <p className="text-red-600 font-bold">
+        aasfadsfsdafdsaf SDAFSADVSA VVSDVSD SDFVCDSAVDSVDSAVDSAV SDAV
+        SDAVS23423EDSFC əƏəəAssÜsssĞğĞ,ŞÇÇIııƏppPOl
+      </p>
+      <p>asadfasd</p>
+      <p className="text-red-600 font-bold">
+        aasfadsfsdafdsaf SDAFSADVSA VVSDVSD SDFVCDSAVDSVDSAVDSAV SDAV
+        SDAVS23423EDSFC əƏəəAssÜsssĞğĞ,ŞÇÇIııƏppPOl
+      </p>
+      <p>asadfasd</p>
+      <p className="text-red-600 font-bold">
+        aasfadsfsdafdsaf SDAFSADVSA VVSDVSD SDFVCDSAVDSVDSAVDSAV SDAV
+        SDAVS23423EDSFC əƏəəAssÜsssĞğĞ,ŞÇÇIııƏppPOl
+      </p>
+      <p>asadfasd</p>
+      <p className="text-red-600 font-bold">
+        aasfadsfsdafdsaf SDAFSADVSA VVSDVSD SDFVCDSAVDSVDSAVDSAV SDAV
+        SDAVS23423EDSFC əƏəəAssÜsssĞğĞ,ŞÇÇIııƏppPOl
+      </p>
+      <p>asadfasd</p>
+    </div>
+  );
+};
+
+export default App;
