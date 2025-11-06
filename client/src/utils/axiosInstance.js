49c2ad95801f9694cae0114f342dae25ea7948ab
elgunakbarli <elgunekberli200@gmail.com>
2025-11-06 23:33:55 +0400

    dep33

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,21 +1,27 @@
+// src/utils/axiosInstance.js
 import axios from 'axios';
 import { BASE_URL } from './apiPaths';
 
 const axiosInstance = axios.create({
-  baseURL: BASE_URL,
+  baseURL: BASE_URL, // məsələn: https://quisor-dev.onrender.com
   timeout: 80000,
-  withCredentials: true,
+  withCredentials: true, // cookie varsa da getsin (opsional)
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
   },
 });
 
+// ⬇️ ƏSAS HİSSƏ: LocalStorage-dakı token-i Authorization başlığına qoy
+axiosInstance.interceptors.request.use((config) => {
+  const t = localStorage.getItem('token');
+  if (t) config.headers.Authorization = `Bearer ${t}`;
+  return config;
+});
+
 axiosInstance.interceptors.response.use(
   (response) => response,
-  (error) => {
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
 
 export default axiosInstance;
